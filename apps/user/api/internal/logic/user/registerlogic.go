7cf1520dee9bac6b23ce0a3190a0f3b316c783b9
Pelz Wang <wangpinze@hotmail.com>
2024-07-13 11:50:52 +0800

    2024-50-13-11

---
+++ apps/user/api/internal/logic/user/registerlogic.go
@@ -0,0 +1,31 @@
+package user
+
+import (
+	"context"
+
+	"ZeZeIM/apps/user/api/internal/svc"
+	"ZeZeIM/apps/user/api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type RegisterLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+// 用户注册
+func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *RegisterLogic {
+	return &RegisterLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *RegisterLogic) Register(req *types.RegisterReq) (resp *types.RegisterResp, err error) {
+	// todo: add your logic here and delete this line
+
+	return
+}
---
+++ apps/user/api/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"ZeZeIM/apps/user/api/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
